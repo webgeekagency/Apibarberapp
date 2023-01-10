491e8a16645f8464d17708bb00487ea57965d492
webgeekagency <121633762+webgeekagency@users.noreply.github.com>
2023-01-10 08:48:42 -0300

    Update Cortes.js

--- src/schemas/Cortes.js
+++ src/schemas/Cortes.js
@@ -3,7 +3,7 @@ import { model, Schema } from 'mongoose'
 const Cortes = new Schema({
     idSalao: String,
     corte: String,
-    valor: Number
+    valor: String
 })
 
 export default new model('Cortes', Cortes)

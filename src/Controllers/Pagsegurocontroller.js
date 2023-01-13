458da17f0b7459c1556af5a7a9a860481d7a5cdf
webgeekagency <121633762+webgeekagency@users.noreply.github.com>
2023-01-13 15:45:58 -0300

    Update Pagsegurocontroller.js

--- src/Controllers/Pagsegurocontroller.js
+++ src/Controllers/Pagsegurocontroller.js
@@ -4,33 +4,47 @@ class Pagsegurocontroller {
     async post(req, res) {
         // const { reference_id, description, amount: { value, currency }, payment_method: { type, installments, capture, soft_descriptor, card: { number, exp_month, exp_year, security_code, holder_name: { name } } } } = req.body
 
-        await axios.post('https://sandbox.api.pagseguro.com/charges', {
+        await axios.post('https://api.pagar.me/core/v5/orders', {
             headers: {
-                Authorization: '351AEF02FA7E4DBEA919A42DC8016637',
-                'Content-Type': 'application/json',
-                //'Access-Control-Allow-Origin': '*'
+                accept: 'application/json',
+                'content-type': 'application/json',
+                Authorization: 'Basic c2tfdGVzdF9vTVIzQjVPQzVmYVc1Sm1qOg==',
             },
-            "reference_id": "ex-00001",
-            "description": "Motivo do pagamento",
-            "amount": {
-                "value": 1000,
-                "currency": "BRL"
+            "items": [
+                {
+                    "amount": 15,
+                    "description": "Barberapp",
+                    "quantity": 1
+                }
+            ],
+            "customer": {
+                "name": "Tony Stark",
+                "email": "avengerstark@ligadajustica.com.br"
             },
-            "payment_method": {
-                "type": "CREDIT_CARD",
-                "installments": 1,
-                "capture": false,
-                "soft_descriptor": "My Store",
-                "card": {
-                    "number": "4111111111111111",
-                    "exp_month": "03",
-                    "exp_year": "2026",
-                    "security_code": "123",
-                    "holder": {
-                        "name": "Jose da Silva"
+            "payments": [
+                {
+                    "payment_method": "credit_card",
+                    "credit_card": {
+                        "recurrence": true,
+                        "installments": 1,
+                        "statement_descriptor": "AVENGERS",
+                        "card": {
+                            "number": "4000000000000010",
+                            "holder_name": "Tony Stark",
+                            "exp_month": 1,
+                            "exp_year": 30,
+                            "cvv": "351",
+                            "billing_address": {
+                                "line_1": "10880, Malibu Point, Malibu Central",
+                                "zip_code": "90265",
+                                "city": "Malibu",
+                                "state": "CA",
+                                "country": "US"
+                            }
+                        }
                     }
                 }
-            }
+            ]
         })
             .then((value) => {
                 return res.json(value)
